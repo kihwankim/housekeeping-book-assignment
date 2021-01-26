a3332917b3c7a97d9f1e3e2d66df33722a33f6be
clack2933 <clack2933>
2021-01-26 17:59:31 +0900

    feat: 집계 출력 코드 추가

--- app/Views/calendar.php
+++ app/Views/calendar.php
@@ -1,195 +1,164 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/vue"></script>
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
-  <meta charset="utf-8"/>
-  <title>housekeeping data</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+    <meta charset="utf-8" />
+    <title>housekeeping data</title>
 </head>
+
 <body>
+    <div id="app">
+        <template>
+            <v-app>
+                <div data-app>
+                    <v-row class="fill-height">
+                        <v-col>
+                            <v-sheet height="64">
+                                <v-toolbar flat>
+                                    <v-btn outlined class="mr-4" color="grey darken-2" @click="setToday">
+                                        Today
+                                    </v-btn>
+                                    <v-btn fab text small color="grey darken-2" @click="prev">
+                                        <v-icon small>
+                                            mdi-chevron-left
+                                        </v-icon>
+                                    </v-btn>
+                                    <v-btn fab text small color="grey darken-2" @click="next">
+                                        <v-icon small>
+                                            mdi-chevron-right
+                                        </v-icon>
+                                    </v-btn>
+                                    <v-toolbar-title v-if="$refs.calendar">
+                                        {{ $refs.calendar.title }}
+                                    </v-toolbar-title>
+                                    <v-spacer></v-spacer>
+                                    <v-menu bottom right>
+                                        <template v-slot:activator="{ on, attrs }">
+                                            <v-btn outlined color="grey darken-2" v-bind="attrs" v-on="on">
+                                                <span>{{ typeToLabel[type] }}</span>
+                                                <v-icon right>
+                                                    mdi-menu-down
+                                                </v-icon>
+                                            </v-btn>
+                                        </template>
+                                        <v-list>
+                                            <v-list-item @click="type = 'day'">
+                                                <v-list-item-title>Day</v-list-item-title>
+                                            </v-list-item>
+                                            <v-list-item @click="type = 'week'">
+                                                <v-list-item-title>Week</v-list-item-title>
+                                            </v-list-item>
+                                            <v-list-item @click="type = 'month'">
+                                                <v-list-item-title>Month</v-list-item-title>
+                                            </v-list-item>
+                                            <v-list-item @click="type = '4day'">
+                                                <v-list-item-title>4 days</v-list-item-title>
+                                            </v-list-item>
+                                        </v-list>
+                                    </v-menu>
+                                </v-toolbar>
+                            </v-sheet>
+                            <v-sheet height="600">
+                                <v-calendar ref="calendar" v-model="focus" color="primary" :events="events"
+                                    :event-color="getEventColor" :type="type" @click:event="showEvent"
+                                    @click:more="viewDay" @click:date="viewDay" @change="updateRange"></v-calendar>
+                                <v-menu v-model="selectedOpen" :close-on-content-click="false"
+                                    :activator="selectedElement" offset-x>
+                                    <v-card color="grey lighten-4" min-width="350px" flat>
+                                        <v-toolbar :color="selectedEvent.color" dark>
+                                            <v-btn icon @click="linkEditPage">
+                                                <v-icon>mdi-pencil</v-icon>
+                                            </v-btn>
+                                            <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+                                        </v-toolbar>
+                                        <v-card-text>
+                                            <span v-html="selectedEvent.price"></span>
+                                            <span>원</span>
+                                            <br />
+                                            <span v-html="(selectedEvent.spentType == 0 ? 'earn' : 'pay')"></span>
+                                        </v-card-text>
+                                        <v-card-actions>
+                                            <v-btn text color="secondary" @click="selectedOpen = false">
+                                                Cancel
+                                            </v-btn>
+                                            <v-btn text color="secondary" @click="requestDeleteData">
+                                                Delete
+                                            </v-btn>
+                                        </v-card-actions>
+                                    </v-card>
+                                </v-menu>
+                            </v-sheet>
+                        </v-col>
+                    </v-row>
+                    <div align="right">
+                        <v-btn @click="overlay = !overlay" elevation="2">
+                            Summary
+                        </v-btn>
+                        <v-btn elevation="2" @click="linkCreatePage">Create</v-btn>
+                    </div>
+                    <v-overlay :absolute="absolute" :value="overlay" :dark=false :light=true>
+                        <v-container>
+                            <v-card color="grey lighten-4" min-width="350px" flat>
+                                <v-toolbar :color="'indigo'" dark>
+                                    <v-toolbar-title>summary</v-toolbar-title>
+                                </v-toolbar>
+                                <v-card-text>
+                                    <v-row>
+                                        <v-col v-if="!isWeekSummary">
+                                            <template>
+                                                <v-row>
+                                                    <v-col>
+                                                        {{ koreanCharacterTypeToLabel['type'] }} 총 입금 :
+                                                        {{ sumOfIncomeMoney() }}
+                                                    </v-col>
+                                                </v-row>
+                                                <v-row>
+                                                    <v-col>
+                                                        {{ koreanCharacterTypeToLabel['type'] }} 총 출금 :
+                                                        {{ sumOfSpentMoney() }}
+                                                    </v-col>
+                                                </v-row>
+                                            </template>
+                                        </v-col>
+                                        <v-col v-if="(type==='month' && isWeekSummary)">
+                                            <template>
+                                                <v-row v-for="(item, index) in sumOfWeekPayment()">
+                                                    <v-col>
+                                                        {{Number(index) + 1}} 째주 입금 {{item[0]}}
+                                                    </v-col>
+                                                    <v-col>
+                                                        {{Number(index) + 1}} 째주 출금 {{item[1]}}
+                                                    </v-col>
+                                                </v-row>
+                                            </template>
+                                        </v-col>
+                                    </v-row>
+                                </v-card-text>
+                                <v-card-actions>
+                                    <v-btn text color="secondary" @click="overlay = false">
+                                        Close
+                                    </v-btn>
+                                    <v-btn text color="secondary" @click="isWeekSummary=!isWeekSummary" v-if="(type==='month')">
+                                        week
+                                    </v-btn>
+                                </v-card-actions>
+                            </v-card>
+                        </v-container>
+                    </v-overlay>
+                </div>
+            </v-app>
+        </template>
+    </div>
 
-<div id="app">
-<template>
-  <v-app>
-  <div data-app>
-  <v-row class="fill-height">
-    <v-col>
-      <v-sheet height="64">
-        <v-toolbar
-          flat
-        >
-          <v-btn
-            outlined
-            class="mr-4"
-            color="grey darken-2"
-            @click="setToday"
-          >
-            Today
-          </v-btn>
-          <v-btn
-            fab
-            text
-            small
-            color="grey darken-2"
-            @click="prev"
-          >
-            <v-icon small>
-              mdi-chevron-left
-            </v-icon>
-          </v-btn>
-          <v-btn
-            fab
-            text
-            small
-            color="grey darken-2"
-            @click="next"
-          >
-            <v-icon small>
-              mdi-chevron-right
-            </v-icon>
-          </v-btn>
-          <v-toolbar-title v-if="$refs.calendar">
-            {{ $refs.calendar.title }}
-          </v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-menu
-            bottom
-            right
-          >
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                outlined
-                color="grey darken-2"
-                v-bind="attrs"
-                v-on="on"
-              >
-                <span>{{ typeToLabel[type] }}</span>
-                <v-icon right>
-                  mdi-menu-down
-                </v-icon>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item @click="type = 'day'">
-                <v-list-item-title>Day</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = 'week'">
-                <v-list-item-title>Week</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = 'month'">
-                <v-list-item-title>Month</v-list-item-title>
-              </v-list-item>
-              <v-list-item @click="type = '4day'">
-                <v-list-item-title>4 days</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </v-toolbar>
-      </v-sheet>
-      <v-sheet height="600">
-        <v-calendar
-          ref="calendar"
-          v-model="focus"
-          color="primary"
-          :events="events"
-          :event-color="getEventColor"
-          :type="type"
-          @click:event="showEvent"
-          @click:more="viewDay"
-          @click:date="viewDay"
-          @change="updateRange"
-        ></v-calendar>
-        <v-menu
-          v-model="selectedOpen"
-          :close-on-content-click="false"
-          :activator="selectedElement"
-          offset-x
-        >
-          <v-card
-            color="grey lighten-4"
-            min-width="350px"
-            flat
-          >
-            <v-toolbar
-              :color="selectedEvent.color"
-              dark
-            >
-              <v-btn icon @click="linkEditPage">
-                <v-icon>mdi-pencil</v-icon>
-              </v-btn>
-              <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
-            </v-toolbar>
-            <v-card-text>
-              <span v-html="selectedEvent.price"></span>
-              <span>원</span>
-              <br/>
-              <span v-html="(selectedEvent.spentType == 0 ? 'earn' : 'pay')"></span>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn
-                text
-                color="secondary"
-                @click="selectedOpen = false"
-              >
-                Cancel
-              </v-btn>
-              <v-btn
-                text
-                color="secondary"
-                @click="requestDeleteData"
-              >
-                Delete
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-menu>
-      </v-sheet>
-    </v-col>
-  </v-row>
-  <div align="right">
-    <v-btn
-      @click="overlay = !overlay"
-      elevation="2"
-    >
-      Summary
-    </v-btn>
-    <v-btn elevation="2" @click="linkCreatePage">Create</v-btn>
-  </div>
-  <v-overlay
-    :absolute="absolute"
-    :value="overlay"
-    :dark=false
-    :light=true
-  >
-    <v-container>
-      <div style="background-color: white; height: 110px; width: 300px; border-radius: 10px" align="center">
-        <div class="mt-5">
-        <v-toolbar-title>
-          {{ koreanCharacterTypeToLabel[type] }} 누적 금액 : {{ sumOfMoney() }}
-        </v-toolbar-title>
-        </div>
-        <br/>
-        <div align="right" class="mr-3">
-          <v-btn
-            @click="overlay = false"
-          >
-              Close
-          </v-btn>
-        </div>
-      </div>
-    </v-container>
-  </v-overlay>
-  </div>
-  </v-app>
-</template>
-</div>
-
-<script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
-<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-<script src="http://localhost/housekeeping-book/public/js/calendar.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    <script src="http://localhost/housekeeping-book/public/js/calendar.js"></script>
 </body>
+
 </html>
--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,130 +1,176 @@
-new Vue({ 
+new Vue({
     el: '#app',
     vuetify: new Vuetify(),
     data: {
-      year: '',
-      month: '',
-      focus: '',
-      absolute: true,
-      overlay: false,
-      type: 'month',
-      typeToLabel: {
-        month: 'Month',
-        week: 'Week',
-        day: 'Day',
-        '4day': '4 Days',
-      },
-      koreanCharacterTypeToLabel: {
-        month: '월',
-        week: '주',
-        day: '일',
-        '4day': '4 일'
-      },
-      selectedEvent: {},
-      selectedElement: null,
-      selectedOpen: false,
-      events: [],
-      colors: ['orange', 'cyan', 'blue', 'indigo', 'deep-purple', 'green', 'grey darken-1'],
-      BASE_URL: 'http://localhost/housekeeping-book/public/index.php'
+        isWeekSummary: false,
+        focus: '',
+        absolute: true,
+        overlay: false,
+        type: 'month',
+        typeToLabel: {
+            month: 'Month',
+            week: 'Week',
+            day: 'Day',
+            '4day': '4 Days',
+        },
+        koreanCharacterTypeToLabel: {
+            month: '월',
+            week: '주',
+            day: '일',
+            '4day': '4 일'
+        },
+        selectedEvent: {},
+        selectedElement: null,
+        selectedOpen: false,
+        events: [],
+        colors: ['orange', 'cyan', 'blue', 'indigo', 'deep-purple', 'green', 'grey darken-1'],
+        BASE_URL: 'http://localhost/housekeeping-book/public/index.php'
     },
-    mounted () {
-      this.$refs.calendar.checkChange();
+    mounted() {
+        this.$refs.calendar.checkChange();
     },
     methods: {
-      linkCreatePage() {
-        window.location.href=`${this.BASE_URL}/home/new`;
-      },
-      viewDay ({ date }) {
-        this.focus = date
-        this.type = 'day'
-      },
-      getEventColor (event) {
-        return event.color
-      },
-      setToday () {
-        this.focus = ''
-      },
-      prev () {
-        this.$refs.calendar.prev()
-      },
-      next () {
-        this.$refs.calendar.next()
-      },
-      showEvent ({ nativeEvent, event }) {
-        this.nowId = event.id;
-        const open = () => {
-          this.selectedEvent = event
-          this.selectedElement = nativeEvent.target
-          setTimeout(() => {
-            this.selectedOpen = true
-          }, 10)
-        }
-        if (this.selectedOpen) {
-          this.selectedOpen = false
-          setTimeout(open, 10)
-        } else {
-          open()
-        }
-        nativeEvent.stopPropagation()
-      },
-      updateRange ({ start, end }) {
-        fetch(`${this.BASE_URL}/data/housekeeps/${start.date}/${end.date}`)
-        .then(res => {
-          if(res.ok){
-            return res.json();
-          }
-          throw new Error("Network reponse was not ok");
-        })
-        .then(json => {
-          const events = [];
-          json.events.forEach(element => {
-            events.push({
-              start: element.use_at,
-              end: element.use_at,
-              color: this.colors[element.spent_type % this.colors.length],
-              timed: false,
-              id: element.id,
-              name: element.description,
-              price: element.price ,
-              spentType: element.spent_type,
-            });
-          });
-          this.events = events;
-        })
-        .catch(error => {console.log(error)});
-      },
-      requestDeleteData() {
-        if(confirm('Are you sure deleting this housekeeping data')){
-          axios.delete(`${this.BASE_URL}/data/delete/${this.nowId}`)
-            .then(res => {
-              const events = [];
-              this.events.forEach(element => {
-                if(element.id != res.data.id){
-                  events.push(element);
+        linkCreatePage() {
+            window.location.href = `${this.BASE_URL}/home/new`;
+        },
+        viewDay({
+            date
+        }) {
+            this.focus = date
+            this.type = 'day'
+        },
+        getEventColor(event) {
+            return event.color
+        },
+        setToday() {
+            this.focus = ''
+        },
+        prev() {
+            this.$refs.calendar.prev()
+        },
+        next() {
+            this.$refs.calendar.next()
+        },
+        showEvent({
+            nativeEvent,
+            event
+        }) {
+            this.nowId = event.id;
+            const open = () => {
+                this.selectedEvent = event
+                this.selectedElement = nativeEvent.target
+                setTimeout(() => {
+                    this.selectedOpen = true
+                }, 10)
+            }
+            if (this.selectedOpen) {
+                this.selectedOpen = false
+                setTimeout(open, 10)
+            } else {
+                open()
+            }
+            nativeEvent.stopPropagation()
+        },
+        updateRange({
+            start,
+            end
+        }) {
+            fetch(`${this.BASE_URL}/data/housekeeps/${start.date}/${end.date}`)
+                .then(res => {
+                    if (res.ok) {
+                        return res.json();
+                    }
+                    throw new Error("Network reponse was not ok");
+                })
+                .then(json => {
+                    const events = [];
+                    json.events.forEach(element => {
+                        events.push({
+                            start: element.use_at,
+                            end: element.use_at,
+                            color: this.colors[element.spent_type % this.colors.length],
+                            timed: false,
+                            id: element.id,
+                            name: element.description,
+                            price: element.price,
+                            spentType: element.spent_type,
+                        });
+                    });
+                    this.events = events;
+                })
+                .catch(error => {
+                    console.log(error)
+                });
+        },
+        requestDeleteData() {
+            if (confirm('Are you sure deleting this housekeeping data')) {
+                axios.delete(`${this.BASE_URL}/data/delete/${this.nowId}`)
+                    .then(res => {
+                        const events = [];
+                        this.events.forEach(element => {
+                            if (element.id != res.data.id) {
+                                events.push(element);
+                            }
+                        })
+                        this.events = events;
+                        this.selectedOpen = false;
+                    })
+                    .catch(error => {
+                        alert("It can not be deleted");
+                    });
+            }
+        },
+        linkEditPage() {
+            window.location.href = `${this.BASE_URL}/home/edit/${this.nowId}`;
+        },
+        sumOfMoney() {
+            let sum = 0;
+            this.events.forEach(element => {
+                if (element.spentType == 0) {
+                    sum += Number(element.price);
+                } else {
+                    sum -= Number(element.price);
                 }
-              })
-              this.events = events;
-              this.selectedOpen = false;
-            })
-            .catch(error => {
-              alert("It can not be deleted");
             });
-        }
-      },
-      linkEditPage() {
-        window.location.href=`${this.BASE_URL}/home/edit/${this.nowId}`;
-      },
-      sumOfMoney() {
-        let sum = 0;
-        this.events.forEach(element => {
-          if(element.spentType == 0){
-            sum += Number(element.price);
-          }else{
-            sum -= Number(element.price);
-          }
-        });
 
-        return sum;
-      }
+            return sum;
+        },
+        sumOfSpentMoney() {
+            let sum = 0;
+            this.events.forEach(element => {
+                if (!(element.spentType == 0)) {
+                    sum += Number(element.price);
+                }
+            });
+
+            return sum;
+        },
+        sumOfIncomeMoney() {
+            let sum = 0;
+            this.events.forEach(element => {
+                if (element.spentType == 0) {
+                    sum += Number(element.price);
+                }
+            });
+
+            return sum;
+        },
+        sumOfWeekPayment() {
+            const result = {
+                0: [0, 0],
+                1: [0, 0],
+                2: [0, 0],
+                3: [0, 0],
+                4: [0, 0],
+            };
+
+            this.events.forEach(element => {
+                const date = new Date(Date.parse(element.start)).getDate();
+                const spentType = element.spentType;
+                result[parseInt(Math.floor((date - 1) / 7))][spentType] += Number(element.price);
+            });
+
+            return result;
+        }
     }
-  })
+})
